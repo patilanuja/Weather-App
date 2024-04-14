fe52b7d98c0b87d307ed3a43a4ac6c5acac028f1
anuja patil <ahpatil@clemson.edu>
2024-04-14 15:18:16 -0400

    Project Upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "50c53a6028msh70cbd4184dbc1a6p1413d7jsnc09822384f5e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0";
+export const WEATHER_API_KEY = "c8fe8f133d7b548abcbd58a8af0cfe94"; 
